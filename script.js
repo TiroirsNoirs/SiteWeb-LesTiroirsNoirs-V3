6e9481799e87e09daa5448c6307c8326cc538223
Les Tiroirs Noirs <roz.kerden@gmail.com>
2026-01-05 15:46:01 +0100

    change ordre des pages et modif logo étoilé

--- script.js
+++ script.js
@@ -99,6 +99,60 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     }
+
+    // ==============================================
+    // 4. EFFET ÉTOILES CONTINU (HOVER)
+    // ==============================================
+    
+    const logo = document.querySelector('.hero-image');
+    let starInterval; // Pour stocker le chronomètre
+
+    if (logo) {
+        // QUAND LA SOURIS ENTRE : On lance la machine
+        logo.addEventListener('mouseenter', () => {
+            starInterval = setInterval(() => {
+                creerEtoile();
+            }, 10); // Une étoile toutes les 10ms (Réduis ce chiffre pour plus d'étoiles)
+        });
+
+        // QUAND LA SOURIS SORT : On coupe le moteur
+        logo.addEventListener('mouseleave', () => {
+            clearInterval(starInterval);
+        });
+    }
+
+    // La fonction qui fabrique une seule étoile
+    function creerEtoile() {
+        const star = document.createElement('div');
+        star.classList.add('magic-star');
+        star.innerHTML = '★'; 
+        
+        // 1. Centre du logo
+        const rect = logo.getBoundingClientRect();
+        const centerX = rect.left + window.scrollX + (rect.width / 2);
+        const centerY = rect.top + window.scrollY + (rect.height / 2);
+
+        // 2. Position de départ (Centre + léger hasard)
+        const randomOffset = 20; 
+        star.style.left = (centerX + (Math.random() - 0.5) * randomOffset) + 'px';
+        star.style.top = (centerY + (Math.random() - 0.5) * randomOffset) + 'px';
+
+        // 3. Direction
+        const x = (Math.random() - 0.5) * 800 + 'px';
+        const y = (Math.random() - 0.5) * 800 + 'px';
+        
+        star.style.setProperty('--tx', x);
+        star.style.setProperty('--ty', y);
+
+        // 4. Style
+        const size = Math.random() * 20 + 10;
+        star.style.fontSize = size + 'px';
+        star.style.transform = `rotate(${Math.random() * 360}deg)`;
+
+        document.body.appendChild(star);
+
+        setTimeout(() => { star.remove(); }, 1200);
+    }
 });
 
 
